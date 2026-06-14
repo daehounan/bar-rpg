31decd5695d686d2f95afe21cd83b461e3edd81f
daehounan <an.daehoun@gmail.com>
2026-06-14 11:59:54 +0900

    chore: update PWA build

---
+++ react/sw.js
@@ -0,0 +1,10 @@
+const PREFIX='bar-react-';
+const CACHE=PREFIX+'v28-3741957c';
+const CORE=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./apple-touch-icon.png", "./favicon-32.png"];
+self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(CORE);}).catch(function(){}));});
+self.addEventListener('activate',function(e){e.waitUntil((async function(){var ks=await caches.keys();await Promise.all(ks.filter(function(k){return k.indexOf(PREFIX)===0&&k!==CACHE;}).map(function(k){return caches.delete(k);}));await self.clients.claim();})());});
+self.addEventListener('fetch',function(e){var r=e.request;if(r.method!=='GET')return;e.respondWith((async function(){
+  var hit=await caches.match(r,{ignoreSearch:true});if(hit)return hit;
+  try{var resp=await fetch(r);if(resp&&resp.ok&&resp.type==='basic'){var c=await caches.open(CACHE);c.put(r,resp.clone());}return resp;}
+  catch(err){var fb=await caches.match('./index.html');return fb||Response.error();}
+})());});
